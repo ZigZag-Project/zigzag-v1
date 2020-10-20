ec7a8704cdcb08f09b1f3d6c93dab05452ed850b
Purushotham MV <purushotham.mv@nokia-bell-labs.com>
2020-10-20 16:40:03 +0200

    adding test case for plotting energy and utilization. This test tends to report min energy to be more than max utilization schedule

---
+++ test/test_plotting_en_ut.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+#################################### HEADER ####################################
+
+# File creation : Thu Oct  8 12:42:00 2020
+# Language : Python3
+
+################################### IMPORTS ####################################
+
+# Standard library
+from os.path import join  # Used for path manipulations
+
+# Internal imports
+from test.utils import zigzag  # Used to run ZigZag with the given files.
+
+##################################### CODE #####################################
+
+# NOTE
+# This file is not only meant as a simple test for ZigZag, but also as a
+# template that should be copy pasted to create other tests.
+
+
+def test_base():
+    """
+    BUG
+    ===
+    This test should always run correctly.
+    """
+    # First, the name of the directory in which ZigZag's input files are
+    # located.
+    directory = "./test/files/plotting_en_ut"
+    # Then, the base name of all the input files we will be working with.
+    settings = "settings.yaml"
+    mapping = "mapping.yaml"
+    memory_pool = "memory_pool.yaml"
+    architecture = "architecture.yaml"
+    # Now we run the zigzag framework and assert that the run is successfull.
+    assert zigzag(
+        settings=join(directory, settings),
+        mapping=join(directory, mapping),
+        memory_pool=join(directory, memory_pool),
+        architecture=join(directory, architecture),
+    )
+
+
+##################################### MAIN #####################################
+
+if __name__ == "__main__":
+    # The actions to perform when this file if called as a script go here
+    pass
+
+##################################### EOF ######################################
